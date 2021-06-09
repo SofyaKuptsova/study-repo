b465eb3b2127525ba6551366d8fd7bcab29f277d
Софья Купцова <n9161389890@gmail.com>
2021-06-09 13:06:48 +0300

    Принцип наследования. Добавлен базовый класс Figure

--- ООП/Snake/Snake/Program.cs
+++ ООП/Snake/Snake/Program.cs
@@ -10,17 +10,22 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetWindowSize(80, 25);
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            //Отрисовка рамки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
-            HorizontalLine hline = new HorizontalLine(5, 10, 8, '+');
-            hline.Drow();
-
-            VerticalLine vline = new VerticalLine(5, 10, 8, '+');
-            vline.Drow();
+            //Отрисовка точек
+            Point p = new Point(1, 3, '*');
+            p.Draw();
 
             Console.ReadLine();
         }
